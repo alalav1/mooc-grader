54da1ee8d54f39b72686bd83823ae12a63278019
Vesa Ala-Laurinaho <vesa.ala-laurinaho@aalto.fi>
2019-11-06 14:30:40 +0200

    Show the correct answer in the feedback only if option set in the exercise yaml

--- access/types/forms.py
+++ access/types/forms.py
@@ -32,7 +32,9 @@ class GradedForm(forms.Form):
             raise ConfigError("Missing exercise configuration from form arguments.")
         self.exercise = kwargs.pop("exercise")
         self.show_correct = kwargs.pop('show_correct') if 'show_correct' in kwargs else False
-        self.show_correct_once = kwargs.pop('show_correct_once') if 'show_correct_once' in kwargs else False
+        self.show_correct_once = False
+        if self.exercise.get('show_correct_once', False):
+            self.show_correct_once = kwargs.pop('show_correct_once') if 'show_correct_once' in kwargs else False
         self.request = kwargs.pop('request') if 'request' in kwargs else None
         kwargs['label_suffix'] = ''
         super(forms.Form, self).__init__(*args, **kwargs)
